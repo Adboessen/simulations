ef883912fa7dd2f2c8b1bfd1c504641837edc3b1
Adboessen <andrewboessen@amdg.rockhursths.edu>
2020-02-13 09:14:50 -0600

    coin flip A

--- coin_flip.py
+++ coin_flip.py
@@ -47,9 +47,28 @@ while i < (numFlips - 1):
     i += 1
 
 
-
 print(f"# of 2 heads in a row = {multipleHeads}")
 print(f"# of 2 tails in a row = {multipleTails}")
+
+countHeads = 0
+countTails = 0
+choice = int(input("Which side are you looking for? (1 for Heads; 0 for Tails): "))
+strLength = int(input("How long is the run you're looking for? "))
+
+if choice == 1:
+    i = 0
+    for i in range(0, numFlips - 1):
+        if flips[i:i+strLength] == [1] * strLength:
+            countHeads += 1
+    print(f"{[1] * strLength} occurs {countHeads} times")
+
+elif choice == 0:
+    i = 0
+    for i in range(0, numFlips - 1):
+        if flips[i:i+strLength] == [0] * strLength:
+            countTails += 1
+    print(f"{[0] * strLength} occurs {countTails} times")
+
 
 
 
