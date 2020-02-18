5daa23817b8791cc2670b01b5ed9539557aba64b
Adboessen <andrewboessen@amdg.rockhursths.edu>
2020-02-18 08:45:07 -0600

    shuffled deck

--- cards.py
+++ cards.py
@@ -1,4 +1,5 @@
 #♠♥♣♦
+import random
 suits = ["♠","♥","♣","♦"]
 deck = []
 
@@ -14,7 +15,17 @@ for i in range(4):
         elif(j == 13):
             value = "K"
         deck.append(f"{value}{suits[i]}")
+print("Sorted: ")
 print(deck)
+
+def randomize (arr, n): 
+    for i in range(n-1,0,-1): 
+        j = random.randint(0,i+1) 
+        arr[i],arr[j] = arr[j],arr[i] 
+    return arr 
+n = len(deck)
+print("Shuffled: ")
+print(randomize(deck, n)) 
                 
 
 
